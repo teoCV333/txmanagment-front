33836ef5348b68dc8ec03c8492440958637b1e1e
MCVdev <mateocarvajalvergara333@gmail.com>
2024-01-14 17:20:40 -0500

    pdf complete

--- src/SearchDetail/index.js
+++ src/SearchDetail/index.js
@@ -3,6 +3,15 @@ import './SearchDetail.css';
 import logo1 from './searchdetaillogo1.png';
 import logo2 from './searchdetaillogo2.png';
 import pdfLogo from './pdf-logo.png';
+import inputBalance from './inputbalance.png';
+import accountInfLogo from './importantaccountinfo.png';
+import table1 from './table1.png';
+import table2 from './table2.png';
+import inputs2 from './inputs2.png';
+import footer from './footer.png';
+import mininput from './mininput.png';
+import equalinput from './equalinput.png';
+import squaredot from './squaredot.png';
 import { useParams, useNavigate } from 'react-router-dom';
 import { jsPDF } from 'jspdf';
 import { AuthModal } from './AuthModal';
@@ -14,10 +23,10 @@ function SearchDetail() {
     const { id } = useParams();
 
     const userAccount = {
-        id: id,
+        id: 1,
         name: 'Fulanito',
         lastname: 'Peréz Durango',
-        accountNumber: '5844082932',
+        accountNumber: id,
         address: '7312 N 21ST ST',
         country: 'Pennsylvania',
         city: 'PHILADELPHIA',
@@ -32,6 +41,15 @@ function SearchDetail() {
     }
 
     const currentDate = new Date();
+
+    var year = currentDate.getFullYear();
+    var month = (currentDate.getMonth() + 1).toString().padStart(2, '0'); // Months are zero-indexed
+    var day = currentDate.getDate().toString().padStart(2, '0');
+    var hours = currentDate.getHours().toString().padStart(2, '0');
+    var minutes = currentDate.getMinutes().toString().padStart(2, '0');
+    var seconds = currentDate.getSeconds().toString().padStart(2, '0');
+
+    const idDate = year + month + day + hours + minutes + seconds;
 
     const formattedDate = currentDate.toLocaleDateString('en-US', {
         year: 'numeric',
@@ -256,6 +274,241 @@ function SearchDetail() {
         doc.text('This account is not currently covered by Overdraft Protection. If you would like more information regarding Overdraft Protection and eligibility requirements', 8, 255);
         doc.text('please call the number listed on your statement or visit your Wells Fargo branch.', 8, 259);
 
+        doc.addPage();
+
+        doc.setFontSize(10);
+        doc.setFont("arial", "normal");
+        doc.text(formattedDate, 15, 25);
+
+        doc.addImage(pdfLogo, 'PNG', pageWidth - 35, 10, 18, 18);
+
+        doc.addImage(accountInfLogo, 'PNG', 10, 55, 10, 10);
+
+        doc.setFont("arial", "bold");
+        doc.setFontSize(17);
+        doc.text('IMPORTANT ACCOUNT INFORMATION', 23, 62);
+
+        doc.setLineWidth(0.2);
+        doc.line(95, 68, 120, 68);
+
+        doc.setFont("arial", "normal");
+        doc.setFontSize(9);
+        doc.text(`
+        Effective on or after April 1, 2021, Wells Fargo will no longer issue temporary debit cards, including Wells Fargo Instant Issue Debit
+        Cards, Wells Fargo Business Instant Issue Debit Cards, and EasyPay Instant Cards. If you need a replacement card, you may request one
+        by signing on to Wells Fargo Online®
+        or calling the number on your statement. Once requested, replacement cards arrive by mail in 5
+        to 7 calendar days. You may add your Wells Fargo Debit Card or EasyPay Card to a Wells Fargo-supported digital wallet on your mobile
+        device so you can make secure, convenient purchases in stores, online, and in apps, and access Wells Fargo ATMs while you wait for
+        your replacement card. Availability may be affected by your mobile carrier's coverage area. Your mobile carrier's message and data rates 
+        may apply. Some ATMs within secure locations may require a physical card for entry.`, 3, 74, {align: 'left'});
+
+        doc.setLineWidth(0.2);
+        doc.line(95, 110, 120, 110);
+
+        doc.setFont("arial", "normal");
+        doc.setFontSize(9);
+        doc.text(`
+        Effective on or after April 1, 2021, the ATM Access Code feature will no longer be available to access your accounts at Wells Fargo ATMs.
+        You may continue to access Wells Fargo ATMs using your Wells Fargo Debit, ATM or EasyPay Card, or with a Wells Fargo-supported
+        digital wallet on your mobile device. For more information about adding your card to a digital wallet, please visit
+        wellsfargo.com/mobile/payments. Availability may be affected by your mobile carrier's coverage area. Your mobile carrier's message
+        and data rates may apply. Some ATMs within secure locations may require a physical card for entry. Note: After the ATM Access Code
+        feature for accessing Wells Fargo accounts is discontinued, the "Use an Access Code" button may continue to be displayed on Wells
+        Fargo ATMs to support other services`, 3, 115, {align: 'left'});
+
+        doc.setLineWidth(0.2);
+        doc.line(95, 147, 120, 147);
+
+        doc.setFont("arial", "normal");
+        doc.setFontSize(9);
+        doc.text(`
+        To provide you with additional flexibility to access accounts, we have increased the daily ATM withdrawal limit on your Wells Fargo
+        Debit, ATM, or EasyPay Card(s) to $1,010. Any card that already has a daily ATM withdrawal limit of $1,010 or more remains the same. To
+        view your card limits any time, sign on at wellsfargo.com/cardcontrol and click on Open Card Details`, 3, 152, {align: 'left'});
+
+
+        doc.addPage();
+
+        doc.setFontSize(10);
+        doc.setFont("arial", "normal");
+        doc.text(formattedDate, 15, 25);
+
+        doc.addImage(pdfLogo, 'PNG', pageWidth - 35, 10, 18, 18);
+
+        doc.setLineWidth(0.6);
+        doc.line(8, 38, pageWidth - 13, 38);
+
+        doc.setFontSize(10);
+        doc.setFont("arial", "bold");
+        doc.text("Worksheet to balance your account", 8, 42);
+
+        doc.setFontSize(9);
+        doc.setFont("arial", "normal");
+        doc.text(`
+        Follow the steps below to reconcile your statement balance with your
+        account register balance. Be sure that your register shows any interest
+        paid into your account and any service charges, automatic payments or
+        ATM transactions withdrawn from your account during this statement
+        period.`, 2, 44, {align: 'left'});
+
+        doc.setLineWidth(0.2);
+        doc.rect(8.5, 64, 3, 3.5);
+        doc.setFont("arial", "bold");
+        doc.setFontSize(9);
+        doc.text('A', 9, 66.5);
+        doc.text('Enter the ending balance', 14, 67);
+        doc.setFont("arial", "normal");
+        doc.text('on this statement.', 49, 67);
+        doc.addImage(inputBalance, 'PNG', 73, 63, 22, 5)
+
+        doc.rect(8.5, 69, 3, 3.5);
+        doc.setFont("arial", "bold");
+        doc.text('B', 9, 71.5);
+        doc.text(`List outstanding deposits and other`, 14, 72, {align: 'left'});
+        doc.text(`credits`, 8, 76, {align: 'left'});
+        doc.setFont("arial", "normal");
+        doc.text(`
+            to your account that do not appear on
+this statement.`, 8.1, 72.2, {align: 'left'})
+        doc.setFont("arial", "bold");
+        doc.text(`Enter the total`, 27.9, 79.3, {align: 'left'})
+        doc.setFont("arial", "normal");
+        doc.text(`in the column`, 48.2, 79.3, {align: 'left'})
+        doc.text(`to the right.`, 8, 83.1, {align: 'left'})
+
+        doc.addImage(table1, 8, 87, 65, 30);
+        doc.addImage(inputs2, 74, 110, 25, 10);
+
+        doc.rect(8.5, 120, 3, 3.5);
+        doc.setFont("arial", "bold");
+        doc.text('C', 9, 122.5);
+        doc.text('Add', 13, 122.5);
+
+        doc.rect(20, 120, 3, 3.5);
+        doc.setFont("arial", "bold");
+        doc.setFontSize(9);
+        doc.text('A', 20.5, 122.5);
+
+        doc.text('and', 24, 122.5);
+
+        doc.rect(30, 120, 3, 3.5);
+        doc.setFont("arial", "bold");
+        doc.setFontSize(9);
+        doc.text('B', 30.5, 122.5);
+
+        doc.setFont("arial", "normal");
+        doc.text("to calculate the subtotal.", 34.5, 122.5);
+
+        doc.rect(8.5, 126, 3, 3.5);
+        doc.setFont("arial", "bold");
+        doc.text('D', 9, 128.6);
+
+        doc.text(`List outstanding checks, withdrawals, and`, 13, 128.6);
+        doc.text(`other debits`, 8, 132.6);
+
+        doc.setFont("arial", "normal");
+        doc.text(`to your account that do not appear`, 25, 132.6);
+        doc.text("on this statement.", 8, 136.6);
+
+        doc.setFont("arial", "bold");
+        doc.text('Enter the total', 32, 136.5);
+        
+        doc.setFont("arial", "normal");
+        doc.text(`in the column`, 53, 136.5);
+        doc.text(`to the right.`, 8, 140.5);
+        
+        doc.addImage(table2, 8, 150, 65, 100);
+        doc.addImage(mininput, 76, 245, 22, 5)
+
+        doc.rect(8.5, 255, 3, 3.5);
+        doc.setFont("arial", "bold");
+        doc.text('E', 9, 257.5);
+        doc.text('Subtract', 13, 257.5);
+
+        doc.rect(26, 255, 3, 3.5);
+        doc.setFont("arial", "bold");
+        doc.setFontSize(9);
+        doc.text('D', 26.5, 257.5);
+
+        doc.text('from', 30, 257.5);
+        
+        doc.rect(38, 255, 3, 3.5);
+        doc.setFont("arial", "bold");
+        doc.setFontSize(9);
+        doc.text('C', 38.5, 257.5);
+
+        doc.setFont("arial", "normal");
+        doc.text("to calculate the", 42.5, 257.5);
+        doc.text(`adjusted ending balance. This amount should be
+the same as the current balance shown in your
+register.`, 9, 261.5)
+
+        doc.addImage(equalinput, 76, 263, 22, 5)
+
+    
+
+
+        doc.line(100, 38, 100, pageHeigth - 30);
+
+        doc.setFontSize(10);
+        doc.setFont("arial", "bold");
+        doc.text("General statement policies for Wells Fargo Bank", 102, 42);
+
+        doc.addImage(squaredot, 101, 48, 1, 1);
+
+        doc.setFontSize(9);
+        doc.text(`To dispute or report inaccuracies in information we have furnished to a
+Consumer Reporting Agency about your accounts.`, 104, 50);
+        doc.setFont("arial", "normal");
+        doc.text(`You have the right to`, 173, 53.8)
+        doc.text(`dispute the accuracy of information that Wells Fargo Bank, N.A. has
+furnished to a consumer reporting agency by writing to us at Overdraft
+Collection and Recovery, P.O. Box 5058, Portland, OR 97208-5058. Please
+describe the specific information that is inaccurate or in dispute and the
+basis for the dispute along with supporting documentation. If you believe
+the information furnished is the result of identity theft, please provide us
+with an identity theft report.`, 104, 57.8)
+
+
+        doc.addImage(squaredot, 101, 88, 1, 1);
+        doc.setFont("aria", "bold");
+        doc.text(`In case of errors or questions about your electronic transfers,`, 104, 90);
+        doc.setFont("arial", "normal");
+        doc.text(`telephone us at the number printed on the front of this statement or write
+us at Wells Fargo Bank, P.O. Box 6995, Portland, OR 97228-6995 as soon as
+you can, if you think your statement or receipt is wrong or if you need more
+information about a transfer on the statement or receipt. We must hear
+from you no later than 60 days after we sent you the FIRST statement on
+which the error or problem appeared.
+        `, 104, 93);
+
+        doc.setFont("arial", "bold");
+        doc.text('1.', 104, 118);
+
+        doc.setFont('arial', "normal");
+        doc.text('Tell us your name and account number (if any).', 108, 118);
+
+        doc.setFont("arial", "bold");
+        doc.text('2.', 104, 122);
+
+        doc.setFont('arial', "normal");
+        doc.text(`Describe the error or the transfer you are unsure about, and explain as
+clearly as you can why you believe it is an error or why you need more
+information.`, 108, 122);
+
+        doc.setFont("arial", "bold");
+        doc.text('3.', 104, 134);
+
+        doc.setFont('arial', "normal");
+        doc.text(`Tell us the dollar amount of the suspected error.`, 108, 134);
+       
+        doc.text(`We will investigate your complaint and will correct any error promptly. If
+we take more than 10 business days to do this, we will credit your account
+for the amount you think is in error, so that you will have the use of the
+money during the time it takes us to complete our investigation`, 104, 140);
+
+        doc.addImage(footer, 105, pageHeigth - 40, 100, 8);
 
         //page
        /*  doc.setFontSize(10);
@@ -271,7 +524,7 @@ function SearchDetail() {
         doc.setFontSize(9);
         doc.text('How to avoid the monthly service fee', 22, 45); */
 
-        doc.save("test-1.pdf");
+        doc.save(`${userAccount.name + '-' + userAccount.lastname}-${idDate}.pdf`);
     };
 
     const watchStatements = () => {
